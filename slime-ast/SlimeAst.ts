773d3896e503516a08f62596fd06bf426fca4d8d
alamhubb <alamhubb@163.com>
2024-11-22 13:20:44 +0800

    init

--- slime-ast/SlimeAst.ts
+++ slime-ast/SlimeAst.ts
@@ -1,3 +1,11 @@
 export interface SlimeAstInterface {
-
+    NumericLiteral: 'NumericLiteral'
+    stringLiteral: 'stringLiteral'
+    booleanLiteral: 'booleanLiteral'
 }
+
+const SlimeAst: SlimeAstInterface = {
+    NumericLiteral: 'NumericLiteral'
+}
+
+export default SlimeAst
