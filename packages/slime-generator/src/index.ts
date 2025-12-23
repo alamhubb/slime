3d2c23263144a6791ede1e0b6d7b8ae99bdf1c5b
alamhubb <alamhubb@163.com>
2025-12-23 14:21:18 +0800

    init

--- packages/slime-generator/src/index.ts
+++ packages/slime-generator/src/index.ts
@@ -1,7 +1,8 @@
 export * from './SlimeAstPrintUtil.ts'
 export * from './SlimeCodeMapping.ts'
-export { default as SlimeCodeMapping } from './SlimeCodeMapping.ts'
 export * from './SlimeGenerator.ts'
+
+export { default as SlimeCodeMapping } from './SlimeCodeMapping.ts'
 export { default as SlimeGenerator } from './SlimeGenerator.ts'
 
 // 导出 deprecated 模块
--- packages/slime-parser/src/index.ts
+++ packages/slime-parser/src/index.ts
@@ -1,10 +1,11 @@
 export * from './SlimeCstToAstUtil.ts'
-export { default as SlimeCstToAstUtil } from './SlimeCstToAstUtil.ts'
 export * from './SlimeParser.ts'
-export { default as SlimeParser } from './SlimeParser.ts'
 export * from './SlimeTokenConsumer.ts'
-export { default as SlimeTokenConsumer } from './SlimeTokenConsumer.ts'
 export * from './SlimeTokens.ts'
+
+export { default as SlimeCstToAstUtil } from './SlimeCstToAstUtil.ts'
+export { default as SlimeParser } from './SlimeParser.ts'
+export { default as SlimeTokenConsumer } from './SlimeTokenConsumer.ts'
 export { default as slimeTokens } from './SlimeTokens.ts'
 
 // 导出 cstToAst 模块
@@ -12,7 +13,8 @@ export * from './cstToAst/index.ts'
 
 // 导出 deprecated 模块
 export * from './deprecated/SlimeJavascriptCstToAstUtil.ts'
+export * from './deprecated/SlimeJavascriptTokens.ts'
+
 export { default as SlimeJavascriptCstToAstUtil } from './deprecated/SlimeJavascriptCstToAstUtil.ts'
-export { SlimeJavascriptTokensObj } from './deprecated/SlimeJavascriptTokens.ts'
 
 
